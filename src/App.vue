7a31429b2538c1bd9a48f604f68448523f2e6a55
JlexaSS <jowe.jlexa@mail.ru>
2022-09-28 16:01:15 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <v-menu/>
+    <h1>Hi, guys</h1>
+
+  </div>
+</template>
+
+<script>
+import vMenu from '@/components/v-menu'
+export default {
+  name: 'app',
+  components: {
+    vMenu
+  }
+}
+</script>
+
+<style lang="scss">
+  .app {
+    display: flex;
+  }
+</style>
---
+++ src/components/v-menu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="menu">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-menu"
+}
+</script>
+
+<style lang="scss">
+  .menu {
+    width: 240px;
+    height: 100vh;
+    background-color: #21282F;
+  }
+</style>
